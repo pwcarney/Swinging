fc4d6af460130518a4a91273f6beaa7d09bcfc83
Patrick <pwcarney@bu.edu>
2018-08-06 22:06:59 -0400

    Added some city generation

---
+++ Assets/Scripts/CityGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Cameras;
+
+public class CityGenerator : MonoBehaviour
+{
+    bool played_player = false;
+    public GameObject player_prefab;
+    public GameObject camera_prefab;
+
+    float ground_size = 40;
+    public GameObject ground_prefab;
+
+    float floor_offset = 4f;
+    float floor_height = 8f;
+    public GameObject floor_prefab;
+
+    void Start()
+    {
+        for (int i = -25; i < 25; i++)
+        {
+            for (int j = -25; j < 25; j++)
+            {
+                Vector3 location = new Vector3(i * ground_size, 0, j * ground_size);
+                GameObject ground = Instantiate(ground_prefab, location, Quaternion.identity);
+
+                if (Random.Range(0, 100) < 10)
+                {
+                    int building_height = Random.Range(1, 10);
+                    for (int k = 0; k < building_height; k++)
+                    {
+                        Instantiate(floor_prefab, location + new Vector3(0, k * floor_height + floor_offset, 0), Quaternion.identity);
+                    }
+                }
+                else if (!played_player && (i > -5 && i < 5) && (j > -5 && j < 5))
+                {
+                    played_player = true;
+                    GameObject player = Instantiate(player_prefab, location, Quaternion.identity);
+
+                    GameObject camera = Instantiate(camera_prefab, location, Quaternion.identity);
+                    camera.GetComponent<FreeLookCam>().Target = player.transform;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,6 @@ public class PlayerController : MonoBehaviour
     Vector3 starting_position;
 
     public float speed;
-    private bool jumping = false;
-    private bool double_jump_ready = true;
     private bool wall_delay = false;
     public float jump_power;
     private Transform ground_check;
@@ -40,7 +38,6 @@ public class PlayerController : MonoBehaviour
                 attached = true;
                 if (GetComponent<SpringJoint>() == null)
                 {
-                    double_jump_ready = true;
                     SetSpring();
                 }
                 GetComponent<SpringJoint>().connectedAnchor = hit.point;
@@ -58,23 +55,12 @@ public class PlayerController : MonoBehaviour
         if (IsGrounded() && Input.GetButtonDown("Jump"))
         {
             body.AddForce(new Vector3(0, jump_power, 0));
-            double_jump_ready = true;
         }
         else if (IsTouchingWall && Input.GetButton("Jump") && !wall_delay)
         {
             wall_delay = true;
             Invoke("DelayWallJump", 0.25f);
             body.AddForce(new Vector3(0, jump_power, 0));
-            double_jump_ready = true;
-        }
-        else if (double_jump_ready && Input.GetButtonDown("Jump"))
-        {
-            attached = false;
-            Destroy(GetComponent<SpringJoint>());
-
-            body.velocity = new Vector3(0f, 0f, 0f);
-            body.AddForce(new Vector3(0, jump_power, 0));
-            double_jump_ready = false;
         }
 
         float moveVertical = Input.GetAxisRaw("Vertical");
@@ -99,7 +85,7 @@ public class PlayerController : MonoBehaviour
 
     private bool IsGrounded()
     {
-        Collider[] overlaps = Physics.OverlapSphere(ground_check.position, 0.3f, ground_mask);
+        Collider[] overlaps = Physics.OverlapSphere(ground_check.position, 0.5f, ground_mask);
         if (overlaps.Length > 0)
         {
             return true;
